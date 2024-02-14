9d4abd15f9233678408496e83e8317c851946911
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-14 22:21:07 +0000

    middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import router from "./router"
+import morgan from "morgan"
 const app=express()
 
 
 
-
+app.use(morgan("dev"))
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
