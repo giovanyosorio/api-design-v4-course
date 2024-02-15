c62ce68462345653d4d5d17fbc55ebbd7e188578
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-15 01:33:48 +0000

    Creating a custom middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express"
 import router from "./router"
 import morgan from "morgan"
+import cors from "cors"
 const app=express()
 
 
 
 app.use(morgan("dev"))
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
