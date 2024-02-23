6c162bffd9e5df80425c838b36e9a03ec4f8e547
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-23 20:53:25 +0000

    Create & Update Product Handlers

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -26,3 +26,37 @@ export const getOneProduct=async(req,res)=>{
     res.json({data:product})
 
 }
+
+
+export const createProduct=async(req,res)=>{
+    const product =await prisma.product.create({
+        data:{
+            name:req.body.name,
+            belongsToId:req.user.id
+        }
+    })
+    res.json({data:product})
+}
+
+
+export const updateProduct=async(req,res)=>{
+    const updateProduct=await prisma.product.update({
+        where:{
+           id_belongsToId:{id: req.params.id,belongsToId:req.user.id}
+        },
+        data:{
+            name:req.body.name
+        }
+    })
+    res.json({data:updateProduct})
+}
+
+export const deleteProduct=async(req,res)=>{
+    const deleteProduct=await prisma.product.delete({
+        where:{
+            id:req.params.id,
+            belongsToId:req.user.id
+        }
+    })
+    res.json({data:deleteProduct})
+}
