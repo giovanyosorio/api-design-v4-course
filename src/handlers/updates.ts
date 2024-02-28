959e8b783e05c46f2e4147d402d610fbe7745489
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-28 21:41:41 +0000

    Create & Delete Handlers

--- src/handlers/updates.ts
+++ src/handlers/updates.ts
@@ -25,11 +25,66 @@ export const getUpdates=async(req,res)=>{
 }
 
 export const createUpdate=async(req,res)=>{
-    
+    const product=await prisma.update.findUnique({
+        where:{
+            id:req.body.id
+        }
+    })
+    if(!product){
+        //does not belong to users
+        return res.json({message:"nope"})
+    }
+    const update=await prisma.update.create({
+        data:req.body
+    })
+    res.json({data:update})
 }
 export const updateUpdate=async(req,res)=>{
-    
+    const products=await prisma.product.findMany({
+        where:{
+            belongsToId:req.user.id,
+        },
+        include:{
+            updates:true
+        }
+    })
+    const updates=products.reduce((allUpdates,product)=>{
+        return [...allUpdates,...product.updates]
+    },[])
+    const match=updates.find(update=>update.id===req.params.id)
+    if(!match){
+      return  res.json({message:"nop"})
+    }
+    const updateUpdate=await prisma.update.update({
+        where:{
+            id:req.params.id
+        },
+        data:req.body
+    })
+    res.json({data:updateUpdate})
 }
 export const deleteUpdate=async(req,res)=>{
+    const products=await prisma.product.findMany({
+        where:{
+            belongsToId:req.user.id,
+        },
+        include:{
+            updates:true
+        }
+    })
+    const updates=products.reduce((allUpdates,product)=>{
+        return [...allUpdates,...product.updates]
+    },[])
+    const match=updates.find(update=>update.id===req.params.id)
+    if(!match){
+      return  res.json({message:"nop"})
+    }
+    const deleted=await prisma.update.delete({
+        where:{
+            id:req.params.id
+        }
+        
+    })
+    res.json({data:deleted})
     
 }
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import {Router} from "express"
 import {body, oneOf, validationResult} from "express-validator"
 import { handleInputErrors } from "./modules/middleware"
 import { createProduct, deleteProduct, getOneProduct, getProducts } from "./handlers/product"
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from "./handlers/updates"
 
 const router=Router()
 
@@ -23,30 +24,21 @@ router.delete("/product/:id",deleteProduct)
  * Update
  */
 
-router.get("/update",(req,res)=>{
-    res.send("hello world")
-})
+router.get("/update",getUpdates)
 
-router.get("/update/:id",(req,res)=>{
-    res.send("hello world")
-})
+router.get("/update/:id",getOneUpdate)
 
 router.put("/update/:id",
 body("title").optional(),
 body("body").optional(),
-body("status").isIn(["IN_PROGRESS","SHIPPED","DEPRECATED"]),
+body("status").isIn(["IN_PROGRESS","SHIPPED","DEPRECATED"]).optional(),
 body("version").
-optional(),(req,res)=>{
-    res.send("hello world")
-})
-router.post("/update",body("title").exists(),
-body("body").exists().isString(),(req,res)=>{
-    res.send("hello world")
-})
+optional(),updateUpdate)
 
-router.delete("/update/:id",(req,res)=>{
-    res.send("hello world")
-})
+router.post("/update",body("title").exists(),
+body("body").exists().isString(),body("productId").exists().isString(),createUpdate)
+
+router.delete("/update/:id",deleteUpdate)
 
 /**
  * Update Point
